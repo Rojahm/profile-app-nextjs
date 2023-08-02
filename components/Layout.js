952ee5b5fe1971f4d40df676e1bad58693590dfe
Rojahm <100220824+Rojahm@users.noreply.github.com>
2023-08-02 13:07:00 +0330

    Head tag imported

--- components/Layout.js
+++ components/Layout.js
@@ -1,13 +1,17 @@
+import Head from "next/head";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 
 const Layout = ({ children }) => {
   return (
-    <div className="container">
-      <Navbar />
-      <div className="content">{children}</div>
-      <Footer />
-    </div>
+    <>
+      <Head></Head>
+      <div className="container">
+        <Navbar />
+        <div className="content">{children}</div>
+        <Footer />
+      </div>
+    </>
   );
 };
 
