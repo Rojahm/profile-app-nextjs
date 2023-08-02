4b580474f0148a06d895ffcad18892ed655dfde9
Rojahm <100220824+Rojahm@users.noreply.github.com>
2023-08-02 13:44:12 +0330

    removed Head/next

--- components/Layout.js
+++ components/Layout.js
@@ -1,11 +1,9 @@
-import Head from "next/head";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 
 const Layout = ({ children }) => {
   return (
     <>
-      <Head></Head>
       <div className="container">
         <Navbar />
         <div className="content">{children}</div>
