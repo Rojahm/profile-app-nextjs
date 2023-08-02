27505caced82e48901c289a87979ed7640bbf3cb
Rojahm <100220824+Rojahm@users.noreply.github.com>
2023-08-02 16:19:44 +0330

    dynamic page to render members details. it will pre render and make all the files on the server

--- pages/members/[id].js
+++ pages/members/[id].js
@@ -1,7 +1,40 @@
-const MemberDetailesPage = () => {
+// render each page before requst, to keep on server
+export const getStaticPaths = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await response.json();
+
+  const paths = data.map((member) => {
+    return {
+      params: { id: member.id.toString() },
+    };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+// get the requested user detailes
+export const getStaticProps = async (context) => {
+  const id = context.params.id;
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/users/${id}`
+  );
+  const data = await response.json();
+
+  return {
+    props: { member: data },
+  };
+};
+
+const MemberDetailesPage = ({ member }) => {
   return (
     <div>
       <h1>Member Detailes</h1>
+      <h2>{member.name}</h2>
+      <p>{member.email}</p>
+      <p>{member.website}</p>
+      <p>{member.address.city}</p>
     </div>
   );
 };
